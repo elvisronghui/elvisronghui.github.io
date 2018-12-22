bb97557aa94c2791de930039d60983419614a533
Elvis <elvisronghui@gmail.com>
2018-12-22 11:05:24 +0800

    Update love.js

--- file/love.js
+++ file/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(1, 1);//0.75, 0.75
             ctx.font = "12px Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Come Baby,快进来玩玩！哈哈", 23, 10);
+            ctx.fillText("快进来玩玩！哈哈", 23, 10);//Come Baby,
             ctx.restore();
         },
         clear: function() {
