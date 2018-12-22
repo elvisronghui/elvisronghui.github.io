fb95e1f56f68c28d779ed1fd5f4b73bb6f1e36fa
Elvis <elvisronghui@gmail.com>
2018-12-22 11:07:41 +0800

    Update love.js

--- file/love.js
+++ file/love.js
@@ -163,12 +163,12 @@
             ctx.scale(scale, scale);
             ctx.moveTo(0, 0);
     	    ctx.lineTo(15, 15);
-    	    ctx.lineTo(60, 15);
+    	    ctx.lineTo(90, 15);
             ctx.stroke();
 
             ctx.moveTo(0, 0);
-            ctx.scale(1, 1);//0.75, 0.75
-            ctx.font = "12px Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
+            ctx.scale(1.1, 1.1);   //0.75, 0.75
+            ctx.font = "12px Verdana";     // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("快进来玩玩！哈哈", 23, 10);//Come Baby,
             ctx.restore();
         },
